6e0b3c7f0c9528567b0953fa1b5929bfb787d4b5
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-23 13:43:11 +0800

    Delete optim directory

--- optim/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Copyright (c) Microsoft Corporation.
-Licensed under the MIT license.
-
-Copied from UNITER
-(https://github.com/ChenRocks/UNITER)
-"""
-from .sched import noam_schedule, warmup_linear, vqa_schedule, get_lr_sched
-from .adamw import AdamW
